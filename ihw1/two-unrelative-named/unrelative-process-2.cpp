74c0df03a579d1f821efc46cfe68b46930263fb9
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-03-17 22:21:24 +0300

    Update unrelative-process-2.cpp

--- ihw1/two-unrelative-named/unrelative-process-2.cpp
+++ ihw1/two-unrelative-named/unrelative-process-2.cpp
@@ -42,14 +42,27 @@ void findSequence(const char* input, int N, char* sequence) {
 
 #define BUFFER_SIZE 5000
 
-int main() {
+int n = 1;
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-n") == 0) {
+            n = atoi(argv[++i]);
+            printf("Inputed n is %d\n", n);
+        }
+    }
+    if (n <= 0) {
+        printf("Number n must be > 0! Program will use n = 1\n");
+        n = 1;
+    }
+
     char buffer[BUFFER_SIZE];
 
     int fd = open(fifo1, O_RDONLY);
     read(fd, buffer, BUFFER_SIZE);
     close(fd);
 
-    findSequence(buffer, 5, buffer);
+    findSequence(buffer, n, buffer);
 
     fd = open(fifo2, O_WRONLY);
     write(fd, buffer, sizeof(buffer));
