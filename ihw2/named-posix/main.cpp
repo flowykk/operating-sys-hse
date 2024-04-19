1427dff09e309f26e2c93a33c2a892cc7a430b37
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-04-19 13:37:06 +0300

    Update main.cpp

--- ihw2/named-posix/main.cpp
+++ ihw2/named-posix/main.cpp
@@ -7,12 +7,9 @@
 #include <random>
 #include <chrono>
 
-// Количество бойцов
 const int numFighters = 6;
-// Имя именованного семафора
 const char* SEMAPHORE_NAME = "/fighter_semaphore";
 
-// Структура для хранения информации о бойце
 struct Fighter {
     int strength;
     bool defeated;
@@ -20,12 +17,9 @@ struct Fighter {
     explicit Fighter(int str) : strength(str), defeated(false) {}
 };
 
-// Функция для симуляции боя между двумя бойцами
 void fight(Fighter& fighter1, Fighter& fighter2, sem_t* semaphore) {
-    // Симуляция боя
     std::this_thread::sleep_for(std::chrono::milliseconds (fighter2.strength / fighter1.strength * 1000));
 
-    // Определение победителя и обновление силы
     printf("Бой: Боец с силой %d против бойца с силой %d\n", fighter1.strength, fighter2.strength);
     if (fighter1.strength > fighter2.strength) {
         fighter1.strength += fighter2.strength;
@@ -37,11 +31,9 @@ void fight(Fighter& fighter1, Fighter& fighter2, sem_t* semaphore) {
         printf("Победитель: Боец с силой %d; Новая сила: %d\n", fighter2.strength - fighter1.strength, fighter2.strength);
     }
 
-    // Сигнализация о завершении боя
     sem_post(semaphore);
 }
 
-// Функция для боя между победителями первых трех боев
 void finalFight(const std::vector<Fighter>& fighters, sem_t* semaphore) {
     int winner1_idx = -1, winner2_idx = -1;
     for (int i = 0; i < numFighters; ++i) {
@@ -57,19 +49,17 @@ void finalFight(const std::vector<Fighter>& fighters, sem_t* semaphore) {
 
     if (winner1_idx != -1 && winner2_idx != -1) {
         std::thread fight_thread(fight, std::ref(const_cast<Fighter&>(fighters[winner1_idx])), std::ref(const_cast<Fighter&>(fighters[winner2_idx])), semaphore);
-        fight_thread.join();  // Присоединение потока боя
+        fight_thread.join();
         sem_wait(semaphore);
         std::cout << "Финальный бой: Боец " << winner1_idx + 1 << " против Бойца " << winner2_idx + 1 << std::endl;
     }
 }
 
 int main() {
-    // Инициализация генератора случайных чисел
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
 
-    // Создание и инициализация массива бойцов
     std::vector<Fighter> fighters;
     for (int i = 0; i < numFighters; ++i) {
         int strength;
@@ -84,32 +74,27 @@ int main() {
     }
     printf("\n");
 
-    // Создание именованного семафора
     sem_t* semaphore = sem_open(SEMAPHORE_NAME, O_CREAT, 0666, 0);
     if (semaphore == SEM_FAILED) {
         std::cerr << "Ошибка создания семафора." << std::endl;
         return 1;
     }
 
-    // Начало битвы
     std::thread fights[3];
     for (int i = 0; i < 3; ++i) {
         fights[i] = std::thread(fight, std::ref(fighters[i * 2]), std::ref(fighters[i * 2 + 1]), semaphore);
     }
 
-    // Ожидание завершения первых трех боев
     for (int i = 0; i < 3; ++i) {
         sem_wait(semaphore);
     }
 
-    // Ожидание завершения боев
     for (int i = 0; i < 3; ++i) {
         fights[i].join();
     }
 
     finalFight(fighters, semaphore);
 
-    // Вывод силы победителя соревнования
     int winner_strength = -1;
     for (const auto& fighter : fighters) {
         if (!fighter.defeated) {
@@ -123,7 +108,6 @@ int main() {
         std::cout << "Все бойцы были побеждены." << std::endl;
     }
 
-    // Закрытие и удаление семафора
     sem_close(semaphore);
     sem_unlink(SEMAPHORE_NAME);
 
