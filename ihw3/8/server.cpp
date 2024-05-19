0e9e1c500908253a8700d20f38621a72571447bf
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-05-19 16:28:49 +0300

    Update server.cpp

--- ihw3/8/server.cpp
+++ ihw3/8/server.cpp
@@ -1,4 +1,3 @@
-#include "echoServer.h"
 #include <cstdio>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -9,31 +8,10 @@
 
 #define MAXPENDING 5
 #define BUFFSIZE 32
-#define RCVBUFSIZE 32   /* Size of receive buffer */
 
 void DieWithError(const char *mess) {
     perror(mess);
     exit(1);
-}
-
-void HandleTCPClient(int clntSocket, int clientsock1)
-{
-    char echoBuffer[RCVBUFSIZE];        /* Buffer for echo string */
-    int recvMsgSize;                    /* Size of received message */
-
-    /* Receive message from client */
-    if ((recvMsgSize = recv(clientsock1, echoBuffer, RCVBUFSIZE, 0)) < 0)
-        DieWithError("recv() failed");
-
-    /* Send received string and receive again until end of transmission */
-    while (recvMsgSize > 0)      /* zero indicates end of transmission */
-    {
-        /* Echo message back to client */
-        if (send(clntSocket, echoBuffer, recvMsgSize, 0) != recvMsgSize)
-            DieWithError("send() failed");
-    }
-
-    close(clntSocket);    /* Close client socket */
 }
 
 int AcceptTCPClient(int servSock) {
@@ -58,15 +36,12 @@ int AcceptTCPClient(int servSock) {
 int main(int argc, char *argv[]) {
     int serversock;
     int clientsock1, clientsock2, clientsock3;
-    pid_t processID;
-    std::vector<int> clntSocks;
-
+    std::vector<int> clients;
     struct sockaddr_in echoServAddr, echoClient;
     char buffer[BUFFSIZE];
     unsigned int recvMsgSize;
     int totalBytesRcvd;
     int messegesReceived = 0;
-    int clientsConntected = 0;
 
     if ((argc < 3) || (argc > 4)) {
         fprintf(stderr, "Usage: %s <Server IP> <Server Port>\n", argv[0]);
@@ -95,56 +70,52 @@ int main(int argc, char *argv[]) {
 
     for(;;) {
         recvMsgSize = sizeof(echoClient);
-        int clntSock;
-        clientsConntected++;
-        if (clientsConntected == 1) {
-            clientsock1 = AcceptTCPClient(serversock);
-        } else {
-            clntSock = AcceptTCPClient(serversock);
-            if ((processID = fork()) < 0)
-                DieWithError("fork() failed");
-            else if (processID == 0)  /* If this is the child process */
-            {
-                close(serversock);   /* Child closes parent socket */
 
-                if ((totalBytesRcvd = recv(clientsock1, buffer, BUFFSIZE, 0)) < 0) {
-                    DieWithError("Failed to receive initial bytes from client");
-                }
-                buffer[totalBytesRcvd] = '\0';
-                bool flag = false;
+        clientsock1 = AcceptTCPClient(serversock);
+        if ((totalBytesRcvd = recv(clientsock1, buffer, BUFFSIZE, 0)) < 0) {
+            DieWithError("Failed to receive initial bytes from client");
+        }
+        buffer[totalBytesRcvd] = '\0';
+        bool flag = false;
 
-                while (totalBytesRcvd > 0) {
-                    messegesReceived++;
-                    if (send(clntSock, buffer, totalBytesRcvd, 0) != totalBytesRcvd)
-                    {
-                        DieWithError("send() failed");
-                    } else {
-                        fprintf(stdout, "Handling server\n");
-                    }
+        int numberOfClients = std::atoi(buffer);
+        clients.resize(numberOfClients - 1, 0);
 
-                    if (messegesReceived >= 6) {
-                        flag = true;
-                        break;
-                    }
+        for (int i = 0; i < numberOfClients - 1; i++) {
+            clients[i] = AcceptTCPClient(serversock);
+        }
 
-                    if ((totalBytesRcvd = recv(clientsock1, buffer, BUFFSIZE, 0)) < 0) {
-                        DieWithError("Failed to receive additional bytes from client");
-                    }
-                    buffer[totalBytesRcvd] = '\0';
-                }
-
-                if (flag) {
-                    break;
-                }
-                //HandleTCPClient(clntSock, clientsock1);
-
-                exit(0);           /* Child process terminates */
+        while (totalBytesRcvd > 0) {
+            messegesReceived++;
+            if ((totalBytesRcvd = recv(clientsock1, buffer, BUFFSIZE, 0)) < 0) {
+                DieWithError("Failed to receive additional bytes from client");
             }
+            buffer[totalBytesRcvd] = '\0';
+
+            for (int i = 0; i < numberOfClients - 1; i++) {
+                if (send(clients[i], buffer, totalBytesRcvd, 0) != totalBytesRcvd) {
+                    DieWithError("Failed to send bytes to client");
+                } else {
+                    fprintf(stdout, "Handling server\n");
+                }
+            }
+
+            if (messegesReceived >= 6) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            break;
         }
 
     }
 
     close(clientsock1);
-    close(clientsock2);
+    for (int i = 0; i < clients.size(); i++) {
+        close(clients[i]);
+    }
+
     return 0;
 }
