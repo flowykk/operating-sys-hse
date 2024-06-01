d200634160d028e51113fdd962e1222fe25ca2dd
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-06-01 19:17:30 +0300

    Create client3.cpp

---
+++ ihw4/6-7/client3.cpp
@@ -0,0 +1,152 @@
+#include <cstdio>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <thread>
+#include <semaphore.h>
+#include <vector>
+#include <iostream>
+
+#define BUFFSIZE 32
+
+const int numFighters = 6;
+const char* SEMAPHORE_NAME = "/fighter_semaphore";
+
+struct Fighter {
+    int strength;
+    bool defeated;
+
+    explicit Fighter(int str) : strength(str), defeated(false) {}
+};
+
+void DieWithError(const char *mess) {
+    perror(mess);
+    exit(1);
+}
+
+void fight(Fighter& fighter1, Fighter& fighter2, sem_t* semaphore) {
+    std::this_thread::sleep_for(std::chrono::milliseconds (fighter2.strength / fighter1.strength * 1000));
+
+    printf("Бой: Боец с силой %d против бойца с силой %d\n", fighter1.strength, fighter2.strength);
+    if (fighter1.strength > fighter2.strength) {
+        fighter1.strength += fighter2.strength;
+        fighter2.defeated = true;
+        printf("Победитель: Боец с силой %d; Новая сила: %d\n", fighter1.strength - fighter2.strength, fighter1.strength);
+    } else {
+        fighter2.strength += fighter1.strength;
+        fighter1.defeated = true;
+        printf("Победитель: Боец с силой %d; Новая сила: %d\n", fighter2.strength - fighter1.strength, fighter2.strength);
+    }
+
+    sem_post(semaphore);
+}
+
+void finalFight(const std::vector<Fighter>& fighters, sem_t* semaphore) {
+    int winner1_idx = -1, winner2_idx = -1;
+    for (int i = 0; i < numFighters; ++i) {
+        if (!fighters[i].defeated) {
+            if (winner1_idx == -1) {
+                winner1_idx = i;
+            } else {
+                winner2_idx = i;
+                break;
+            }
+        }
+    }
+
+    if (winner1_idx != -1 && winner2_idx != -1) {
+        std::thread fight_thread(fight, std::ref(const_cast<Fighter&>(fighters[winner1_idx])), std::ref(const_cast<Fighter&>(fighters[winner2_idx])), semaphore);
+        fight_thread.join();
+        sem_wait(semaphore);
+        std::cout << "Финальный бой: Боец " << winner1_idx + 1 << " против Бойца " << winner2_idx + 1 << std::endl;
+    }
+}
+
+void sim(std::vector<Fighter> fighters) {
+    sem_t* semaphore = sem_open(SEMAPHORE_NAME, O_CREAT, 0666, 0);
+    if (semaphore == SEM_FAILED) {
+        std::cerr << "Ошибка создания семафора." << std::endl;
+        return;
+    }
+
+    std::thread fights[3];
+    for (int i = 0; i < 3; ++i) {
+        fights[i] = std::thread(fight, std::ref(fighters[i * 2]), std::ref(fighters[i * 2 + 1]), semaphore);
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        sem_wait(semaphore);
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        fights[i].join();
+    }
+
+    finalFight(fighters, semaphore);
+
+    int winner_strength = -1;
+    for (const auto& fighter : fighters) {
+        if (!fighter.defeated) {
+            winner_strength = fighter.strength;
+            break;
+        }
+    }
+    if (winner_strength != -1) {
+        std::cout << "Сила победителя соревнования: " << winner_strength << std::endl;
+    } else {
+        std::cout << "Все бойцы были побеждены." << std::endl;
+    }
+
+    sem_close(semaphore);
+    sem_unlink(SEMAPHORE_NAME);
+}
+
+int main(int argc, char *argv[]) {
+    int sock;
+    struct sockaddr_in serverAddr, fromAddr;
+    char buffer[BUFFSIZE];
+    int totalBytesRcvd;
+    socklen_t fromSize = sizeof(fromAddr);
+    std::vector<Fighter> fighters;
+    int messegesReceived = 0;
+
+    if ((argc < 3) || (argc > 4)) {
+        fprintf(stderr, "USAGE: client <server_ip> <port>\n");
+        exit(1);
+    }
+
+    if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        DieWithError("Failed to create socket");
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
+    serverAddr.sin_port = htons(atoi(argv[2]));
+
+    printf("Waiting for the server to send data...\n");
+
+    for(;;) {
+        // Receive data from the server
+        if ((totalBytesRcvd = recvfrom(sock, buffer, BUFFSIZE - 1, 0, (struct sockaddr *) &fromAddr, &fromSize)) <= 0) {
+            DieWithError("Failed to receive initial bytes from server");
+        }
+
+        // Null-terminate the totalBytesRcvd data
+        buffer[totalBytesRcvd] = '\0';
+
+        int energy = std::atoi(buffer);
+        fighters.emplace_back(energy);
+
+        messegesReceived++;
+        if (messegesReceived >= 6) {
+            sim(fighters);
+            break;
+        }
+    }
+
+    close(sock);
+    return 0;
+}
