a20ed8cb3de9a59771b2e700542dda521948641d
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-06-01 19:17:55 +0300

    Update client2.cpp

--- ihw4/6-7/client2.cpp
+++ ihw4/6-7/client2.cpp
@@ -37,15 +37,12 @@ int main(int argc, char *argv[]) {
     printf("Waiting for the server to send data...\n");
 
     while (true) {
-        // Receive data from the server
         if ((totalBytesRcvd = recvfrom(sock, buffer, BUFFSIZE - 1, 0, (struct sockaddr *) &fromAddr, &fromSize)) <= 0) {
             DieWithError("Failed to receive initial bytes from server");
         }
 
-        // Null-terminate the totalBytesRcvd data
         buffer[totalBytesRcvd] = '\0';
 
-        // Print out the totalBytesRcvd data
         printf("Received and Send to Server3: %s", buffer);
 
         messagesReceived++;
